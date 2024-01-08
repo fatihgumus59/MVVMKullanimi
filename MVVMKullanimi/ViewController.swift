27837ba889f1a9a3ddedac6aa3d2573cfb549c03
Fatih Gumus <fatihgumus72@gmail.com>
2024-01-08 20:49:00 +0300

    start

--- MVVMKullanimi/ViewController.swift
+++ MVVMKullanimi/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelSonuc: UILabel!
+    @IBOutlet weak var sayi1Label: UITextField!
+    @IBOutlet weak var sayi2Label: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        labelSonuc.text = "0"
+        
+        
     }
 
-
+    @IBAction func topla(_ sender: Any) {
+        if let alinanSayi1 = sayi1Label.text,let alinanSayi2 = sayi2Label.text{
+            if let sayi1 = Int(alinanSayi1),let sayi2 = Int(alinanSayi2){
+                
+                let sonuc = sayi1+sayi2
+                
+                labelSonuc.text = "Sonuc : \(sonuc)"
+                
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func carp(_ sender: Any) {
+        if let alinanSayi1 = sayi1Label.text,let alinanSayi2 = sayi2Label.text{
+            if let sayi1 = Int(alinanSayi1),let sayi2 = Int(alinanSayi2){
+                
+                let sonuc = sayi1*sayi2
+                
+                labelSonuc.text = "Sonuc : \(sonuc)"
+                
+            }
+            
+        }
+    }
+    
 }
 
