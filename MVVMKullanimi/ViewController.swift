bd37958e04439d2b208af20630eb3b9dc4d385b5
Fatih Gumus <fatihgumus72@gmail.com>
2024-01-08 20:15:05 +0300

    Initial Commit

---
+++ MVVMKullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMKullanimi
+//
+//  Created by Fatih Gümüş on 8.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
